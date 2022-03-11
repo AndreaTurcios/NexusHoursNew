ce1e4833a8bf0298e591cb42d163f0ae4818b040
AndreaTurcios <turciosbarquero@gmail.com>
2022-03-11 08:16:15 -0600

    idk controlador

---
+++ Controllers/Cliente.cs
@@ -0,0 +1,56 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using SistemaMVC_Demo.Models.Entidad;
+using SistemaMVC_Demo.Models;
+
+namespace SistemaMVC_Demo.Controllers
+{
+    public class Cliente : Controller
+    {
+        // GET: NexusWorkHoursS
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(proyectos proyecto)
+        {
+            try
+            {
+                using (NexusWorkHoursSEntities1 db = new NexusWorkHoursSEntities1())
+                {
+                    db.proyectos.Add(proyecto);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Edit(int id, proyectos proyecto)
+        {
+            try
+            {
+                using (NexusWorkHoursSEntities1 db = new NexusWorkHoursSEntities1())
+                {
+                    db.Entry(proyecto).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+    }
+}
